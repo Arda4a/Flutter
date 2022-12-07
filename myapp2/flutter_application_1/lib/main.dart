75c667e37de566af3307575d77b8e471fbd57785
Arda4a <119483512+Arda4a@users.noreply.github.com>
2022-12-07 08:08:51 +0300

    Toplama

---
+++ myapp2/flutter_application_1/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/nalan.dart';
+import 'nalan.dart';
+
+void main(List<String> args) {
+  runApp(const App());
+}
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Benim Projem",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Toplama(),
+    );
+  }
+  }
