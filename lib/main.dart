92f6d48c5ea1d080edb9b48a13d4f22dbd5dd04e
Arda4a <119483512+Arda4a@users.noreply.github.com>
2023-03-17 21:48:32 +0300

    TodoList Remake

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'todolist.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '9174-Arda ',
+      theme: ThemeData(primarySwatch: Colors.red),
+     home: const Todolist(),
+    );
+  }
+}
